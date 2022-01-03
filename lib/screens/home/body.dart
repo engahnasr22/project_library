1afa09dec962667a29a8f2715c398f8d25bba2ca
Ahmed Nasr <55181203+engahnasr22@users.noreply.github.com>
2022-01-03 03:09:39 +0300

    Update body.dart

--- lib/screens/home/body.dart
+++ lib/screens/home/body.dart
@@ -8,65 +8,81 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Column(
-      children: <Widget>[
+      children: [
         Container(
           height: size.height * 0.2,
           child: Stack(
-            children: <Widget>[
-               Container(
-                 width: size.width,
-                  height: size.height * 0.2 - 27,
-                  decoration: const BoxDecoration(
-                  color: kDefaultBackgroundColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36)
-                  )
+            children: [
+              Container(
+                width: size.width,
+                height: size.height * 0.2 - 27,
+                decoration: const BoxDecoration(
+                    color: kDefaultBackgroundColor,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(36),
+                        bottomRight: Radius.circular(36))),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: Row(
+                    children: [
+                      const Text(
+                        "Hello Ahmed",
+                        style: TextStyle(color:kPrimaryColor,fontFamily: 'Tajawal', fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      Image.network(
+                        "https://sm2rtweb.com/app/wp-content/uploads/2022/01/favicon-sm-1.png",
+                        width: 55,
+                        height: 55,
+                      )
+                    ],
                   ),
                 ),
+              ),
               Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                alignment: Alignment.center,
-                margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                height: 54,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: const Offset(0, 10),
-                      blurRadius: 50,
-                      color: kDefaultBackgroundColor.withOpacity(0.23)
-                    )
-                  ]
-                ),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "search",
-                          hintStyle: TextStyle(
-                            color: kDefaultBackgroundColor.withOpacity(0.5),
-                           ),
-                           enabledBorder: InputBorder.none,
-                           focusedBorder: InputBorder.none
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                    height: 54,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                              offset: const Offset(0, 10),
+                              blurRadius: 50,
+                              color: kDefaultBackgroundColor.withOpacity(0.23))
+                        ]),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                                hintText: "search",
+                                hintStyle: TextStyle(
+                                  color:
+                                      kDefaultBackgroundColor.withOpacity(0.5),
+                                ),
+                                enabledBorder: InputBorder.none,
+                                focusedBorder: InputBorder.none),
+                          ),
                         ),
+                        const Icon(
+                          Icons.search,
+                          color: kDefaultBackgroundColor,
+                        )
+                      ],
                     ),
-                    ),
-                    const Icon(Icons.search,
-                    color: kDefaultBackgroundColor,)
-                  ],
-                ),
-              )
-              )
+                  ))
             ],
           ),
-          color: kDefaultBackgroundColor,
+          color: kPrimaryColor,
         )
       ],
     );
